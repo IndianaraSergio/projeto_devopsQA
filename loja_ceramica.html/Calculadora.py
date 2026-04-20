0222bc012421932f54d56c68605ecaf789a1cd3a
Indianara Sergio <indianaracsergio@gmail.com>
2026-04-20 15:41:30 -0300

    feat: adicionando trava de segurança para limite de horas do forno

--- loja_ceramica.html/Calculadora.py
+++ loja_ceramica.html/Calculadora.py
@@ -2,5 +2,8 @@ def calcular_custo_peca(gramas_argila, horas_forno):
     if gramas_argila <= 0:
         return 0
     
-    # R$ 0,02 por grama + R$ 4,00 por hora de forno hipotético prpra ser mais fácil 
+    # QA Inteligente: O forno não pode operar mais de 12h por segurança
+    if horas_forno > 12:
+        return "Erro: Limite de horas do forno excedido"
+        
     return (gramas_argila * 0.02) + (horas_forno * 4.0)
